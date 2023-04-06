a3142e25675cd968bb1e604409852a4eded119b2
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-04-06 12:59:41 -0400

    f   - start of navbar layout and functionality

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,12 +12,23 @@ const initialState = {
 // eslint-disable-next-line react/prop-types
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+  const [screenSize, setScreenSize] = useState(undefined)
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true })
+  }
 
   return (
     <StateContext.Provider
       value={{
         activeMenu,
-        setActiveMenu
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize
       }}
     >
       {children}
